af2b2dd7dca0273ea4b91be2c335b65097a5b7ae
Blaise Liu <blaise.liu@hotmail.com>
2023-04-19 08:42:31 -0400

    clean up

--- M3U8Scraper/Program.cs
+++ M3U8Scraper/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         if (args.Length != 2)
         {
-            Console.WriteLine("Usage: MyApp arg1 arg2");
+            Console.WriteLine("Usage: M3U8Scraper.exe url filename");
             return;
         }
 
@@ -18,7 +18,7 @@ class Program
 
         using var libVlc = new LibVLC();
 
-        const string prefix = "Daily.Wire.Exodus.with.Jordan.Peterson.";
+        const string prefix = "";
         Console.WriteLine($"Scrape {prefix}{filename}");
 
         using var player = new MediaPlayer(libVlc);
@@ -50,38 +50,3 @@ class Program
         Console.ReadKey();
     }
 }
-
-
-
-
-
-//var libVLC = new LibVLC();
-
-////var libVLC = new LibVLC("--no-xlib");
-//var mediaPlayer = new MediaPlayer(libVLC);
-
-//const string m3u8 = "https://manifest-gcp-us-east4-vop1.cfcdn.media.dailywire.com/00e0284DgpUVUxeeKKOyEECy8M5VXCc1TKTg58rcLLD01ffYy1eDvv6GThGJrORG00GPBZXLa8BkY4ZA3022tDe6C2g/rendition.m3u8?cdn=cloudflare&expires=1680978240&skid=default&signature=NjQzMWIxNDBfZmRlZTNkM2M1YTI3NWE0NGExODc5YzYzNzBlYjQwNGIzZTdlYWJlYTE1NTc1YTk0OTY1NTg1MDRmOTA2YWZiYQ==&vsid=F8mYo00I39PdO9vh6z24qxmwh7KXNWWgmqzp32KUSxSmti016UnDQYnuzoSWtRV611ALlZ9sKb501M";
-
-//var media = new Media(libVLC, new Uri(m3u8));
-//mediaPlayer.Play(media);
-
-//// Subscribe to the TimeChanged event to show download progress
-//media.DurationChanged += (sender, e) =>
-//{
-//    Console.WriteLine($"Downloaded {e.Duration} seconds of the video");
-//};
-
-//await Task.Delay(5000); // wait for 5 seconds
-//mediaPlayer.Stop();
-//var outputMedia = new Media(libVLC, new Uri(m3u8));
-//outputMedia.AddOption(
-//    ":sout=#transcode{vcodec=h264,vb=800,acodec=mp3,ab=128}" +
-//    ":standard{access=file,mux=mp4,dst=" + "output_dw.mp4" + "}");
-
-//mediaPlayer.Play(outputMedia);
-
-//await Task.Delay(5000); // wait for 5 seconds
-//mediaPlayer.Stop();
-
-//Console.WriteLine("done!");
-//Console.ReadLine();
